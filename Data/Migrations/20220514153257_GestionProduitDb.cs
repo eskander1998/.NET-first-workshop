355b9755b9a23a9a6d8e8edaba26683295a66543
unknown <eskander.jguirim@esprit.tn>
2022-06-11 13:34:56 +0100

    first commit

---
+++ Data/Migrations/20220514153257_GestionProduitDb.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GP.Data.Migrations
+{
+    public partial class GestionProduitDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    Cin = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Prenom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.Cin);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MyCategories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MyName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MyCategories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Providers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsApproved = table.Column<bool>(type: "bit", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Providers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MyName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    DateProd = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoryId = table.Column<int>(type: "int", nullable: true),
+                    PType = table.Column<int>(type: "int", nullable: false),
+                    IsBiological = table.Column<int>(type: "int", nullable: false),
+                    Herbs = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LabName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MyCity = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MyStreet = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_Products_MyCategories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "MyCategories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Factures",
+                columns: table => new
+                {
+                    ProductFk = table.Column<int>(type: "int", nullable: false),
+                    ClientFk = table.Column<int>(type: "int", nullable: false),
+                    DateAchat = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Prix = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Factures", x => new { x.ProductFk, x.ClientFk, x.DateAchat });
+                    table.ForeignKey(
+                        name: "FK_Factures_Client_ClientFk",
+                        column: x => x.ClientFk,
+                        principalTable: "Client",
+                        principalColumn: "Cin",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Factures_Products_ProductFk",
+                        column: x => x.ProductFk,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Providings",
+                columns: table => new
+                {
+                    ProductsProductId = table.Column<int>(type: "int", nullable: false),
+                    ProvidersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Providings", x => new { x.ProductsProductId, x.ProvidersId });
+                    table.ForeignKey(
+                        name: "FK_Providings_Products_ProductsProductId",
+                        column: x => x.ProductsProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Providings_Providers_ProvidersId",
+                        column: x => x.ProvidersId,
+                        principalTable: "Providers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Factures_ClientFk",
+                table: "Factures",
+                column: "ClientFk");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Providings_ProvidersId",
+                table: "Providings",
+                column: "ProvidersId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Factures");
+
+            migrationBuilder.DropTable(
+                name: "Providings");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Providers");
+
+            migrationBuilder.DropTable(
+                name: "MyCategories");
+        }
+    }
+}
